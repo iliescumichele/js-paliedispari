d01917aad46c8c80cecb6c28c8271b53d7201daf
iliescumichele <iliescumichele@gmail.com>
2022-04-20 18:11:47 +0200

    function to check if a word is palindrome almost done, need fix for output

--- script.js
+++ script.js
@@ -2,14 +2,20 @@
 const btnPalindroma = document.getElementById('palindroma');
 const btnPariDispari = document.getElementById('pari_dispari');
 
+
+
+
+
 btnPalindroma.addEventListener('click', function(){
-    const word = "ingegni";
+    const word = prompt("Inserire una parola");
 
 
     //richiamo funzione che mi dice se una parola è palindorma o  no
     isPalidroma(word);
 
 });
+
+
 
 
 
@@ -28,5 +34,29 @@ function isPalidroma(parola){
     const arrOfLetters = parola.split("");
     console.log(arrOfLetters);
 
-    
+    const arrOfLettersReversed = reverseWord(parola);
+    console.log(arrOfLettersReversed);
+
+    let flag = true;
+
+    for(let i=0; i<arrOfLetters.lenght ; i++){
+        if(arrOfLetters[i] !== arrOfLettersReversed[i]){
+            flag = false;
+            break;
+        } 
+    }
+
+    if(flag == true){
+        console.log("la parola è palindroma");
+    }  else{
+        console.log("la parola NON è palindroma");
+    }
+
+
 }
+
+
+//funzione che ritorna un array in ordine contrario
+function reverseWord(parola){
+    return parola.split("").reverse();
+};
